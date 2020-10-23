09d784537d5d0be6f4a593627812a5ae9b4fa6fb
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-10-23 19:18:56 -0400

    - Added some more get methods.

--- server.js
+++ server.js
@@ -4,7 +4,22 @@ const app = express();
 
 app.get("/", function(req, res)
 {
-  response.send("<h1>Hello, world!</h1>");
+  res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/contact", function(req, res)
+{
+  res.send("Contact me at: myemail@myemail.com");
+});
+
+app.get("/about", function(req, res)
+{
+  res.send("Here is a lot of stuff about me");
+});
+
+app.get("/hobbies", function(req, res)
+{
+  res.send("Chilling");
 });
 
 app.listen(3000, function()
